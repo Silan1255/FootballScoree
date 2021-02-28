2ec7c73e6455af040dce7c6d4bd6bed1a2eba3cc
Silanboran <boransilan907@gmail.com>
2021-02-28 19:27:16 +0300

    -giriş yap ve kayıt ol ekranlarında validation kontrolü yapıldı.

--- app/src/main/java/com/example/footballscore/GirisEkrani.kt
+++ app/src/main/java/com/example/footballscore/GirisEkrani.kt
@@ -1,33 +1,43 @@
 package com.example.footballscore
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Patterns
 import android.view.LayoutInflater
-import android.view.inputmethod.InputBinding
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.footballscore.databinding.ActivityGirisEkraniBinding
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_giris_ekrani.*
+import kotlinx.android.synthetic.main.activity_kayit_ol.*
 
 
 class GirisEkrani : AppCompatActivity() {
 
     lateinit var binding: ActivityGirisEkraniBinding
+    lateinit var sharedPreferences: SharedPreferences
+    var userMail: String? = ""
+    var userPassword: String? = ""
+    var isRemembered: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGirisEkraniBinding.inflate(LayoutInflater.from(applicationContext))
         setContentView(binding.root)
 
-        binding.btnGirisYapiniz.setOnClickListener {
-            intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-        }
+        sharedPreferences = getSharedPreferences("com.example.footballscore", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        isRemembered = sharedPreferences.getBoolean(IS_CHECKED, false)
+        userMail = sharedPreferences.getString("userMail", "")
+        userPassword = sharedPreferences.getString("userPassword", "")
+
+        initScreen()
+
         binding.txtKayitYapmadiysaniz.setOnClickListener {
             intent = Intent(applicationContext, KayitOl::class.java)
             startActivity(intent)
@@ -35,66 +45,72 @@ class GirisEkrani : AppCompatActivity() {
 
         binding.apply {
             btn_giris_yapiniz.setOnClickListener {
-                when {
-                    TextUtils.isEmpty(edt_mail_adresi.text.toString().trim {
-                        it <= ' '
-                    }) -> {
-                        Toast.makeText(
-                            this@GirisEkrani,
-                            "Lütfen Mail adresinizi giriniz.",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    TextUtils.isEmpty(edt_sifre.text.toString().trim {
-                        it <= ' '
-                    }) -> {
-                        Toast.makeText(
-                            this@GirisEkrani,
-                            "Lütfen şifre giriniz.",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
+                if (isValid()) {
+                    val MailAdresi: String = edt_mail_adresi.text.toString().trim()
+                    val sifre: String = edt_sifre.text.toString().trim()
 
-                    else -> {
-                        val MailAdresi: String = edt_mail_adresi.text.toString().trim {
-                            it <= ' '
-                        }
-                        val sifre: String = edt_sifre.text.toString().trim {
-                            it <= ' '
-                        }
+                    FirebaseAuth.getInstance()
+                        .signInWithEmailAndPassword(MailAdresi, sifre)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+                                Toast.makeText(this@GirisEkrani, "kayıt işlemini başarıyla tamamladınız.", Toast.LENGTH_LONG).show()
 
-
-                        FirebaseAuth.getInstance()
-                            .signInWithEmailAndPassword(MailAdresi, sifre)
-                            .addOnCompleteListener { task ->
-                                if (task.isSuccessful) {
-                                    Toast.makeText(
-                                        this@GirisEkrani,
-                                        "kayıt işlemini başarıyla tamamladınız.",
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                    val intent = Intent(this@GirisEkrani, MainActivity::class.java)
-                                    intent.flags =
-                                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    intent.putExtra(
-                                        "user_id",
-                                        FirebaseAuth.getInstance().currentUser!!.uid
-                                    )
-                                    intent.putExtra("email_id", MailAdresi)
-                                    startActivity(intent)
-                                    finish()
-                                } else {
-                                    Toast.makeText(
-                                        this@GirisEkrani,
-                                        task.exception!!.message.toString(),
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                }
+                                val intent = Intent(this@GirisEkrani, MainActivity::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id", FirebaseAuth.getInstance().currentUser!!.uid)
+                                intent.putExtra("email_id", MailAdresi)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(this@GirisEkrani, "Böyle bir kullanıcı bulunamadı, mail adresi veya şifrenizi doğru girdiğinizden emin olun.".toString(), Toast.LENGTH_LONG).show()
                             }
-                    }
+                        }
                 }
             }
         }
+    }
+
+
+    private fun initScreen() {
+        binding.edtMailAdresi.setText(userMail)
+        binding.edtSifre.setText(userPassword)
+        binding.cbBeniUnutma.isChecked = isRemembered
 
     }
+
+    private fun isValid(): Boolean {
+        var isCheckNull = true
+        if (edt_mail_adresi.text.isEmpty()) {
+            Toast.makeText(this@GirisEkrani, "Lütfen Mail adresinizi giriniz.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+        }
+        if (edt_sifre.text.isEmpty()) {
+            Toast.makeText(this@GirisEkrani, "Lütfen Sifrenizi giriniz.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+        }
+        if (isEmailValid(edt_mail_adresi.text.toString()) == false) {
+            Toast.makeText(this@GirisEkrani, "Lütfen Mail Adresiniz doğru formatta giriniz.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+        }
+        if (edt_sifre.text.length < 6) {
+            Toast.makeText(this@GirisEkrani, "Şifreniz en az 6 karakterden oluşmaktadır.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+
+        }
+
+        return isCheckNull
+    }
+
+    fun isEmailValid(email: CharSequence?): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    companion object {
+        const val IS_CHECKED = "isChecked"
+    }
 }
+
+
+//Mail adresi aynı olduğu zaman kayıt olurken - Zaten var bu mail adresi
+//Kullanıcı yoksa böyle bir kullanıcı bulunmamaktadır. Lütfen kayıt olunuz :)
--- app/src/main/java/com/example/footballscore/KayitOl.kt
+++ app/src/main/java/com/example/footballscore/KayitOl.kt
@@ -1,10 +1,13 @@
 package com.example.footballscore
 
 import android.app.Application
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.widget.Toast
 import com.example.footballscore.databinding.ActivityGirisEkraniBinding
@@ -14,94 +17,111 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_giris_ekrani.*
 import kotlinx.android.synthetic.main.activity_kayit_ol.*
 
 
 class KayitOl : AppCompatActivity() {
     lateinit var binding: ActivityKayitOlBinding
+    lateinit var sharedPreferences: SharedPreferences
+
+    var userMail: String? = ""
+    var userPassword: String? = ""
+    var userName: String? = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityKayitOlBinding.inflate(LayoutInflater.from(applicationContext))
         setContentView(binding.root)
 
+        sharedPreferences = getSharedPreferences("com.example.footballscore", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        userMail = sharedPreferences.getString("userMail", "")
+        userPassword = sharedPreferences.getString("userPassword", "")
+        userName = sharedPreferences.getString("userName", "")
+        //initScreen()
+
+
+        binding.txtKayitYaptiysaniz.setOnClickListener {
+            intent = Intent(applicationContext, GirisEkrani::class.java)
+            startActivity(intent)
+        }
+
+
         binding.apply {
 
-            txt_kayit_yaptiysaniz.setOnClickListener {
-                intent = Intent(applicationContext, GirisEkrani::class.java)
-                startActivity(intent)
-            }
             btn_kayit_ol.setOnClickListener {
+                if (isValid()) {
+                    val MailAdresi: String = kayitMail.text.toString().trim()
+                    val sifre: String = kayitSifre.text.toString().trim()
 
 
-                when {
-                    TextUtils.isEmpty(kayit_mail.text.toString().trim {
-                        it <= ' '
-                    }) -> {
-                        Toast.makeText(
-                            this@KayitOl,
-                            "Lütfen Mail adresinizi giriniz...",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    TextUtils.isEmpty(kayit_sifre.text.toString().trim {
-                        it <= ' '
-                    }) -> {
-                        Toast.makeText(
-                            this@KayitOl,
-                            "Lütfen şifre giriniz.",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    TextUtils.isEmpty(kayit_kullanici_adi.text.toString().trim {
-                        it <= ' '
-                    }) -> {
-                        Toast.makeText(
-                            this@KayitOl,
-                            "Lütfen kullanıcı adınızı  giriniz.",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    else -> {
-                        val MailAdresi: String = kayit_mail.text.toString().trim {
-                            it <= ' '
-                        }
-                        val KullaniciAdi: String = kayit_kullanici_adi.text.toString().trim {
-                            it <= ' '
-                        }
-                        val sifre: String = kayit_sifre.text.toString().trim {
-                            it <= ' '
-                        }
+                    FirebaseAuth.getInstance()
+                        .createUserWithEmailAndPassword(MailAdresi, sifre)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+                                if (task.isSuccessful) {
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                                    Toast.makeText(this@KayitOl, "kayıt işlemini başarıyla tamamladınız.", Toast.LENGTH_LONG).show()
+                                    val intent = Intent(this@KayitOl, GirisEkrani::class.java)
 
-
-                        FirebaseAuth.getInstance()
-                            .createUserWithEmailAndPassword(MailAdresi, sifre)
-                            .addOnCompleteListener(
-                                OnCompleteListener<AuthResult> { task ->
-                                    if (task.isSuccessful) {
-                                        val firebaseUser: FirebaseUser = task.result!!.user!!
-                                        Toast.makeText(
-                                            this@KayitOl,
-                                            "kayıt işlemini başarıyla tamamladınız.",
-                                            Toast.LENGTH_LONG
-                                        ).show()
-                                        val intent = Intent(this@KayitOl, GirisEkrani::class.java)
-                                        intent.flags =
-                                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                        intent.putExtra("user_id", firebaseUser.uid)
-                                        intent.putExtra("email_id", MailAdresi)
-                                        startActivity(intent)
-                                        finish()
-                                    } else {
-                                        Toast.makeText(
-                                            this@KayitOl,
-                                            task.exception!!.message.toString(),
-                                            Toast.LENGTH_LONG
-                                        ).show()
-                                    }
-                                })
-                    }
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra("user_id", firebaseUser.uid)
+                                    intent.putExtra("email_id", MailAdresi)
+                                    startActivity(intent)
+                                    finish()
+                                } else {
+                                    Toast.makeText(this@KayitOl, task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                                }
+                            })
                 }
             }
         }
     }
+   /* private fun initScreen() {
+        binding.kayitKullaniciAdi.setText(userMail)
+        binding.kayitSifre.setText(userPassword)
+        binding.kayitKullaniciAdi.setText(userName)
+
+
+    }
+
+    */
+
+    private fun isValid(): Boolean {
+        var isCheckNull = true
+        if (kayit_mail.text.isEmpty()) {
+            Toast.makeText(this@KayitOl, "Lütfen Mail adresinizi giriniz.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+        }
+        else if  (kayit_sifre.text.isEmpty()) {
+            Toast.makeText(this@KayitOl, "Lütfen Sifrenizi giriniz.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+        }
+        else if (kayit_kullanici_adi.text.isEmpty()) {
+            Toast.makeText(this@KayitOl, "Lütfen kullanıcı adınızı  giriniz.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+        }
+        else if (isEmailValid(kayit_mail.text.toString()) == false) {
+            Toast.makeText(this@KayitOl, "Lütfen Mail Adresiniz doğru formatta giriniz.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+        }
+       else  if (kayit_sifre.text.length < 6) {
+            Toast.makeText(this@KayitOl, "Şifreniz en az 6 karakterden oluşmaktadır.", Toast.LENGTH_LONG).show()
+            isCheckNull = false
+
+        }
+
+        return isCheckNull
+    }
+
+    fun isEmailValid(email: CharSequence?): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    companion object {
+        const val IS_CHECKED = "isChecked"
+    }
+
 }
