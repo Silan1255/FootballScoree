2f6984d6e4a0313eda51ba06c052d74d571e0434
Silanboran <boransilan907@gmail.com>
2021-04-27 16:50:42 +0300

    -Tahmiin et sayfası düzenlendi.

--- app/src/main/java/com/example/footballscore/viewModel/FootballFragmentViewModel.kt
+++ app/src/main/java/com/example/footballscore/viewModel/FootballFragmentViewModel.kt
@@ -2,7 +2,10 @@ package com.example.footballscore.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.footballscore.model.ResultResponse
 import com.example.footballscore.model.skor_tahmin.GetTahminMaclar.GetTahminMaclarItem
+import com.example.footballscore.model.skor_tahmin.SkorTahminEt
+import com.example.footballscore.model.user.RegisterUserItem
 import com.example.footballscore.sevis.FutbolAPIServis
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -20,6 +23,7 @@ class FootballFragmentViewModel : ViewModel() {
     }
 
     val takimlerTahmin = MutableLiveData<List<GetTahminMaclarItem>>()
+    val TahminEtResponse: MutableLiveData<ResultResponse> = MutableLiveData()
 
     private fun GetData() {
         dispoosable.add(
@@ -28,6 +32,21 @@ class FootballFragmentViewModel : ViewModel() {
                 .subscribeWith(object : DisposableSingleObserver<List<GetTahminMaclarItem>>() {
                     override fun onSuccess(t: List<GetTahminMaclarItem>) {
                         takimlerTahmin.value = t
+                    }
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                    }
+                })
+        )
+    }
+    fun newUserTakim(body: SkorTahminEt) {
+        dispoosable.add(
+            futbolAPIServis.setNewTakim(body)
+                 .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<ResultResponse>() {
+                    override fun onSuccess(response: ResultResponse) {
+                        TahminEtResponse.value = response
                     }
                     override fun onError(e: Throwable) {
                         e.printStackTrace()
